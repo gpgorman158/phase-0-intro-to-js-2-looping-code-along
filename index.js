ddc70fc8b14e17b8a5f0ef45981de6c659d91055
Gerald Gorman <gpgorman158@gmail.com>
2022-01-21 22:54:52 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+function writeCards(namesArray, eventName){
+    const messages = [];
+    for (let g = 0; g < namesArray.length; g++){
+        //add stuff to a new array and return this array
+        
+        messages.push(`Thank you, ${namesArray[g]}, for the wonderful ${eventName} gift!`);
+    }
+    return messages;
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"));
+
+function countDown(number) {
+    
+    while (number > 0){
+        console.log(number);
+        number--;
+    }
+    console.log(number);
+}
+countDown(10);
